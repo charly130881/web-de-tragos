7336625812da3f50e99625939333ee195733712a
charly <charly130881@gmail.com>
2022-11-16 18:21:34 -0300

    Actualizacion

--- web-de-tragos/bartender/bartender/settings.py
+++ web-de-tragos/bartender/bartender/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web_de_tragos',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
