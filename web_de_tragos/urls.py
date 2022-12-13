c9dc9e55807c7df829b8db4bdbd890e0d91b6bc4
charly <charly130881@gmail.com>
2022-12-13 12:15:51 -0300

    desarrollo

--- web_de_tragos/urls.py
+++ web_de_tragos/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,4 +9,5 @@ urlpatterns = [
     path('about/',views.about,name='about'),
     path('tragos/',views.tragos,name='tragos'),
     path('contacto/',views.contacto,name='contacto'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
